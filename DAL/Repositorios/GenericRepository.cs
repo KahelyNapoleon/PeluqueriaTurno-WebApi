5b46b23716b228cc228c07e590fe88b7b5c5e276
Aguirre Gabriel <AguirreGabrielNahuel@hotmail.com>
2026-02-02 18:04:07 -0300

    Implementacion de los Repositorios, interfaces y clases.

--- DAL/Repositorios/GenericRepository.cs
+++ DAL/Repositorios/GenericRepository.cs
@@ -1,4 +1,4 @@
-﻿//using DAL.Data;
+﻿using DAL.Data;
 using DAL.Repositorios.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -12,42 +12,50 @@ namespace DAL.Repositorios
 {
     public class GenericRepository<T> : IGenericRepository<T> where T:class
     {
-        //private readonly DbSet<T> _dbSet;
-        //private readonly TurnoServicioPeluqueriaContext _dbContext;
-        //public GenericRepository(TurnoServicioPeluqueriaContext dbContext)
-        //{
-        //    _dbSet = dbContext.Set<T>();
-        //    _dbContext = dbContext;
-        //}
+        private readonly DbSet<T> _dbSet;
+        private readonly ApplicationDbContext _dbContext;
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            _dbSet = dbContext.Set<T>();
+            _dbContext = dbContext;
+        }
 
-        //public async Task Add(T TEntity)
-        //{
-        //    await _dbSet.AddAsync(TEntity);
-        //    _dbContext.SaveChanges();
-        //}
+        public async Task Add(T TEntity)
+        {
+            _dbContext.Entry(TEntity).State = EntityState.Added;
+           // await _dbSet.AddAsync(TEntity);
+            await SaveChangesAsync();
+        }
 
-        //public async Task<IEnumerable<T>> GetAll()
-        //{
-        //    var entities = await _dbSet.ToListAsync();
-        //    return entities;
-        //}
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            var entities = await _dbSet.ToListAsync();
+            return entities;
+        }
 
-        //public async Task<T> GetById(int id)
-        //{
-        //    var entity = await _dbSet.FindAsync(id);
-        //    return entity!;
-        //}
+        public async Task<T> GetById(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            return entity!;
+        }
 
-        //public void Remove(T TEntity)
-        //{
-        //    _dbSet.Remove(TEntity);
-        //}
+        public async Task Remove(T TEntity)
+        {
+            _dbContext.Entry(TEntity).State = EntityState.Deleted;
+            await SaveChangesAsync();
+        }
 
-        ////correguir las advertencias.
-        //public void Update(T TEntity)
-        //{
-        //    _dbSet.Update(TEntity);
-        //    _dbContext.SaveChanges();
-        //}
+        //correguir las advertencias.
+        public async Task Update(T TEntity)
+        {
+            _dbContext.Entry(TEntity).State = EntityState.Modified;
+            await SaveChangesAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+
     }
 }
---
+++ DAL/Repositorios/PagoRepository.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Data;
+using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositorios
+{
+    public class PagoRepository(ApplicationDbContext dbContext) : GenericRepository<Pago>(dbContext)
+    {
+    }
+}
